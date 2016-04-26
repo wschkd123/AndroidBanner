a10cd10d6f9285429f86d5ddf173bec53ed5c6b0
wsc <wschkd@qq.com>
2016-04-26 11:54:56 +0800

    remove title

--- app/src/main/java/com/wsc/banner/MainActivity.java
+++ app/src/main/java/com/wsc/banner/MainActivity.java
@@ -1,14 +1,14 @@
 package com.wsc.banner;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Window;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        requestWindowFeature(Window.FEATURE_NO_TITLE);//去标题
         super.onCreate(savedInstanceState);
         setContentView(new BannerView(this));
     }
